62f66b86a677525f4c014bbba4e20766c60151ab
Alexey <@home-laptop>
2023-06-02 18:56:10 +0300

    *

---
+++ web_app_market/src/main/java/by/koroza/zoo_market/web/tag/PaginationTag.java
@@ -0,0 +1,26 @@
+package by.koroza.zoo_market.web.tag;
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+@SuppressWarnings("serial")
+public class PaginationTag extends TagSupport {
+
+	@Override
+	public int doStartTag() throws JspException {
+		return super.doStartTag();
+	}
+
+	@Override
+	public int doEndTag() throws JspException {
+
+		return super.doEndTag();
+	}
+
+	@Override
+	public int doAfterBody() throws JspException {
+
+		return super.doAfterBody();
+	}
+
+}
--- web_app_market/src/main/webapp/jsp/pages/market_page/pets/products_pets_page.jsp
+++ web_app_market/src/main/webapp/jsp/pages/market_page/pets/products_pets_page.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="jakarta.tags.core"%>
+<%@ taglib uri="customtags" prefix="pgn"%>
 <%@page import="by.koroza.zoo_market.web.command.name.FilterName"%>
 <%@page import="by.koroza.zoo_market.web.command.name.InputName"%>
 <%@page import="by.koroza.zoo_market.web.command.name.CommandName"%>
