f1a46995a361b6e6afcb779b82419e363c7a2954
Alexey <@home-laptop>
2023-07-28 22:38:16 +0300

    *

--- web_app_market/src/main/java/by/koroza/zoo_market/main/Main.java
+++ web_app_market/src/main/java/by/koroza/zoo_market/main/Main.java
@@ -1,15 +1,5 @@
 package by.koroza.zoo_market.main;
 
-import java.sql.CallableStatement;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
-
-import by.koroza.zoo_market.dao.pool.ConnectionPool;
-import by.koroza.zoo_market.dao.pool.ProxyConnection;
-
 public class Main {
 
 	public static void main(String[] args) {
